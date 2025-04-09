f56fcaa8df7c19dbe407f52ff3a387d671552820
Spimy <williamlaw.3001@gmail.com>
2025-04-10 01:05:39 +0800

    📦 NEW: Implement Navbar component with navigation items and branding

---
+++ client/src/lib/components/Navbar.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	interface NavItem {
+		label: string;
+		href: string;
+		icon: string;
+	}
+
+	interface Props {
+		brandingIconUrl: string;
+	}
+
+	let { brandingIconUrl }: Props = $props();
+
+	// Array of navigation items
+	const navItems: NavItem[] = [
+		{ label: 'Overview', href: '/overview', icon: '/icons/overview.svg' },
+		{ label: 'Chat', href: '/chat', icon: '/icons/chat.svg' },
+		{ label: 'Inventory', href: '/inventory', icon: '/icons/inventory.svg' },
+		{ label: 'Alerts', href: '/alerts', icon: '/icons/alerts.svg' },
+		{ label: 'Account', href: '/account', icon: '/icons/user.svg' }
+	];
+
+	const handleLogout = () => {};
+</script>
+
+<header class="bg-primary text-tertiary px-3 py-6">
+	<!-- Navbar -->
+	<nav class="flex h-full flex-col items-center space-y-8">
+		<!-- Branding -->
+		<a href="/overview" class="flex flex-col items-center space-y-2">
+			<img src={brandingIconUrl} alt="Brand Logo" class="h-16 w-16 object-contain" />
+			<span class="text-lgsemi">Burger Factory</span>
+		</a>
+
+		<!-- Navigation Items -->
+		<ul class="flex flex-col space-y-6">
+			{#each navItems as { label, href, icon }}
+				<li class="flex flex-col items-center">
+					<a
+						{href}
+						class="{page.url.pathname.includes(href)
+							? 'bg-highlight'
+							: ''} hover:bg-highlight flex w-full flex-col items-center rounded-lg p-4 text-center transition duration-300 ease-in-out"
+					>
+						<img src={icon} alt="{label} Icon" />
+						<span class="text-mdsemi">{label}</span>
+					</a>
+				</li>
+			{/each}
+		</ul>
+
+		<!-- Log Out Button -->
+		<button
+			onclick={handleLogout}
+			class="text-mdsemi mt-auto w-full cursor-pointer rounded-lg bg-red-800 py-3 text-white transition duration-300 hover:bg-red-700"
+		>
+			Log Out
+		</button>
+	</nav>
+</header>
--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+	import { page } from '$app/state';
+	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="grid h-screen grid-cols-[auto_1fr]">
+	{#if page.data.session}
+		<Navbar brandingIconUrl="https://static.spimy.dev/logos/character.png" />
+	{/if}
+
+	<div>
+		{@render children()}
+	</div>
+</div>
