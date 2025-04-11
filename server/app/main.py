c791364f195a540f2781a4d9211b798f0c1d62ba
Spimy <williamlaw.3001@gmail.com>
2025-04-11 17:15:42 +0800

    👌 IMPROVE: Initialise database on app startup lifespan

--- server/app/main.py
+++ server/app/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from contextlib import asynccontextmanager
 from routes import ollama, item, merchant, restaurant_review, keyword
 import uvicorn
 
-from models import create_db
+from models import create_db_and_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
 
 app = FastAPI(
-    title="FastAPI Server", description="A basic FastAPI server", version="1.0.0"
+    title="FastAPI Server", description="A basic FastAPI server", version="1.0.0",
+    lifespan=lifespan
 )
 
 app.include_router(ollama.router)
@@ -30,7 +37,5 @@ app.add_middleware(
 async def root():
     return {"message": "Welcome to the FastAPI server!"}
 
-
 if __name__ == "__main__":
-    create_db()
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
